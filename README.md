f5cfbc17c4b802a4f0449982b66651d00b221df7
Rama <ramawidyadhanabhagaskoro@gmail.com>
2024-06-14 21:05:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation
+
+
+# Internet Access for WSL2
+1. Run `sudo VMEXEC_PATH=$(pwd)/wsl-vm GVPROXY_PATH=$(pwd)/wsl-gvproxy.exe ./wsl-vpnkit` from default root path. Make sure this script is running on the terminal.
+2. Open a new terminal to do stuff.
---
+++ manon-chat-interface/prompts.py
@@ -0,0 +1,11 @@
+BASE_TEMPLATE = """
+You are a friendly and professional AI assistant. 
+You are an expert in the domain of machinery and manufacturing and helps the user to understand whether
+a machine is able to manufacture a part as well as the reason if they aren't compatible.
+Use the following context information to answer the question at the end. Try to reply as shortly as possible.
+
+Context: {context}
+
+Question: {question}
+Answer:
+"""

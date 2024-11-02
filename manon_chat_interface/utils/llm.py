55cc4c581a3ec95e462a0a1dc53dffc1cc1b3648
Rama <ramawidyadhanabhagaskoro@gmail.com>
2024-11-02 12:46:46 +0100

    Changed prompt template and added entity recognition to generation

--- manon_chat_interface/utils/llm.py
+++ manon_chat_interface/utils/llm.py
@@ -1,27 +1,36 @@
 from openai import OpenAI
 from strictjson import strict_json
-from dotenv import load_dotenv
 
-import os
+import chromadb
 
-def generate_sparql_query(path_to_graph, question, **kwargs):
-    # Open the file in read mode with utf-8 encoding
+def generate_sparql_query(path_to_graph, question, mode):
+    # Load turtle file for system prompt context
     with open(path_to_graph, 'r', encoding='utf-8') as file:
         ttl_content = file.read()
 
+    # Get entity metadata
+    path_to_vectorstore = "../data/vectorstores/flight_vectorstore"
+    client = chromadb.PersistentClient(path=f"{path_to_vectorstore}")
+    collection = client.get_or_create_collection("flight_collection")
+
+    entities = entity_recognition(question)
+
+    embeddings = collection.query(
+        query_texts=entities, n_results=1
+    )
+
     query = strict_json(
-        system_prompt=CONTEXT_RETRIEVAL_SYSTEM_PROMPT.format(
+        system_prompt=TEXT2SPARQL_SYSTEM_PROMPT.format(
             context=ttl_content
         ),
-        user_prompt=CONTEXT_RETRIEVAL_USER_PROMPT.format(
-            question=question
+        user_prompt=TEXT2SPARQL_USER_PROMPT.format(
+            question=question, metadata=str(embeddings)
         ),
         output_format={
             "sparql_query": "Executable sparql query string.", 
             "explanation": "Explanation of what the sparql query does."
         },
-        llm=invoke_llm,
-        **kwargs
+        llm=invoke_huggingface if mode == "huggingface" else invoke_llm
     )
 
     return query
@@ -56,7 +65,7 @@ def entity_recognition(input: str) -> dict:
     return response
 
 
-def invoke_llm(system_prompt: str, user_prompt: str, **kwargs):
+def invoke_llm(system_prompt: str, user_prompt: str):
     """Calls the LLM without history.
     LLM function call to be passed to strictjson.strict_json(). It is advised not to change the parameters.
     Refer to https://github.com/tanchongmin/strictjson/blob/main/strictjson/base.py#L319
@@ -87,7 +96,7 @@ def invoke_llm(system_prompt: str, user_prompt: str, **kwargs):
     return response.choices[0].message.content
 
 
-def invoke_huggingface(system_prompt: str, user_prompt: str, **kwargs):
+def invoke_huggingface(system_prompt: str, user_prompt: str):
     """Calls the LLM without history.
     LLM function call to be passed to strictjson.strict_json(). It is advised not to change the parameters.
     Refer to https://github.com/tanchongmin/strictjson/blob/main/strictjson/base.py#L319
@@ -106,7 +115,7 @@ def invoke_huggingface(system_prompt: str, user_prompt: str, **kwargs):
     ]   
 
     # Insert your API key here. Environ settings not set up, key needs to be hard-coded.
-    client = OpenAI(base_url="https://api-inference.huggingface.co/v1/", api_key=API_KEY)
+    client = OpenAI(base_url="https://api-inference.huggingface.co/v1/", api_key="xxx")
 
     print("Generating with huggingface model...")
 
@@ -142,20 +151,22 @@ Input: {input}
 ########################################################################
 ## SPARQL generation ###################################################
 ########################################################################
-CONTEXT_RETRIEVAL_SYSTEM_PROMPT = """
+TEXT2SPARQL_SYSTEM_PROMPT = """
 You are a helpful assistant with expertise in SPARQL. Consider the following RDF graph in Turtle syntax:
     {context}
 Write a SPARQL for the question given by the user following the restrictions:
 -Use only prefixes provided in the RDF graph;
--Use only classes and properties defined in the RDF graph
+-Use only classes and properties defined in the RDF graph;
+-Use the IRIs provided in the object metadata given along the question to construct the query;
 -Declare non-essential properties to the question as OPTIONAL if needed; 
 -DO NOT use specific resources in the query; Declare filters on strings (like labels and names) as filter operations over the REGEX function using the case-insensitive flag.
 
 Your output should be in JSON format, categorizing the identified terms into "sparql_query" and "explanation". The output should be strictly the JSON object without any additional commentary or explanation.
 """
 
-CONTEXT_RETRIEVAL_USER_PROMPT = """
+TEEXT2SPARQL_USER_PROMPT = """
 Question: {question}
+Object metadata: {metadata}
 """
 
 ########################################################################

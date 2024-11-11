55baf49488caaf289556b0ebc22648cfe8cc0b28
Rama <ramawidyadhanabhagaskoro@gmail.com>
2024-11-11 08:36:02 +0100

    Added triple2text evaluation

--- triple2text.py
+++ triple2text.py
@@ -1,5 +1,6 @@
 from manon_chat_interface.utils.sparql import *
 from manon_chat_interface.utils.llm import * 
+from manon_chat_interface.utils.utils import * 
 
 import json
 import os
@@ -8,7 +9,7 @@ import os
 mode = "huggingface"
 model = "Llama-3.1-70B" # This variable is for folder naming only.
 
-def triple2text(question, question_id, triples, mode):
+def triple2text(question, question_id, triples, answer, mode):
     if mode=="huggingface":
         gen_results = invoke_huggingface(
             system_prompt=TRIPLE2TEXT_SYSTEM_PROMPT.format(triples=triples),
@@ -25,8 +26,21 @@ def triple2text(question, question_id, triples, mode):
     os.makedirs(base_path, exist_ok=True)
     file_path = os.path.join(base_path, f"{question_id}.json")
 
+    # Calculate metrics
+    bleu = calculate_bleu(gen_results, answer)
+    rouge_1 = calculate_rouge_n(gen_results, answer, n=1)
+    rouge_2 = calculate_rouge_n(gen_results, answer, n=2)
+    meteor = calculate_meteor(gen_results, answer)
+    BERTscore = calculate_bert_score(gen_results, answer)
+
     output_data = {
-        "gen_results": gen_results
+        "gen_results": gen_results,
+        "answer": answer,
+        "bleu": bleu,
+        "rouge_1": rouge_1,
+        "rouge_2": rouge_2,
+        "meteor": meteor,
+        "BERTscore": BERTscore
     }
 
     # Save the generated results to a JSON file
@@ -43,8 +57,10 @@ for index, item in enumerate(data):
     question = item["question"]
     question_id = item["id"]
     triples = item["triples"]
+    answer = item["answer"]
 
     triple2text(question=question, 
                 question_id=question_id, 
                 triples=triples,
+                answer=answer,
                 mode=mode)
